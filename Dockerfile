37a290b928501c0a68dd87cec722e25e1d828556
REX-nk <jfd2si7a324r@gmail.com>
2023-01-26 07:17:14 +0900

    lesson 47 check.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM python:3.9-alpine3.13
-LABEL mainteiner="londonappdeveloper.com"
+LABEL maintainer="londonappdeveloper.com"
 
 ENV PYTHONUNBUFFERED 1
+
 COPY ./requirements.txt /tmp/requirements.txt
 COPY ./requirements.dev.txt /tmp/requirements.dev.txt
 COPY ./app /app
@@ -26,6 +27,5 @@ RUN python -m venv /py && \
         django-user
 
 ENV PATH="/py/bin:$PATH"
+
 USER django-user
-
-
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,9 +1,10 @@
 """
 Django command to wait for the database to be available.
 """
-
 import time
-from psycopg2 import OperationalError as Psycopg2Error
+
+from psycopg2 import OperationalError as Psycopg2OpError
+
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
@@ -19,7 +20,8 @@ class Command(BaseCommand):
             try:
                 self.check(databases=['default'])
                 db_up = True
-            except (Psycopg2Error, OperationalError):
+            except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
+
         self.stdout.write(self.style.SUCCESS('Database available!'))
